88dbd07b068592ca1dee6dc09698aead959c625d
mapache17 <mota.170903@gmail.com>
2023-04-01 18:41:46 -0500

    UserService metodos retornan objeto UserEntity

--- src/main/java/com/example/jpa_bank/service/UserService.java
+++ src/main/java/com/example/jpa_bank/service/UserService.java
@@ -1,28 +1,24 @@
 package com.example.jpa_bank.service;
 
 import com.example.jpa_bank.controller.dto.UserDto;
+import com.example.jpa_bank.entity.AccountEntity;
 import com.example.jpa_bank.entity.UserEntity;
 import com.example.jpa_bank.repository.AccountRepository;
 import com.example.jpa_bank.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class UserService {
     private UserRepository userRepository;
     private AccountRepository accountRepository;
-    public String createUser(UserDto userDto) {
-        userRepository.save(new UserEntity(userDto.getDocument(),userDto.getName(),userDto.getLastName(),userDto.getDateCreated()));
-        return "An user was created";
+    public UserEntity createUser(UserDto userDto) {
+        return userRepository.save(new UserEntity(userDto.getDocument(),userDto.getName(),userDto.getLastName(),userDto.getDateCreated()));
     }
-    public String consultAccounts(int documentUser) {
-        try {
-        UserEntity actualUser = userRepository.findById(documentUser).orElse(new UserEntity());
-        return "The user: "+actualUser.getName()+" has: "+accountRepository.getAllAccounts(documentUser);
-        }
-        catch (Exception e){
-        return "the user does not exist or does not have an account";
-        }
+    public List<AccountEntity> consultAccounts(int documentUser) {
+        return accountRepository.getAllAccounts(documentUser);
     }
 }
