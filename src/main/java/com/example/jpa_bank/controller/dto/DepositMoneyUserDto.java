b81627d64a62456dbb781a692489522876a50b5a
Jhoan <84868357+Oswe-gif@users.noreply.github.com>
2023-04-03 09:55:31 -0500

    Change the DTOs files

--- src/main/java/com/example/jpa_bank/controller/dto/DepositMoneyUserDto.java
+++ src/main/java/com/example/jpa_bank/controller/dto/DepositMoneyUserDto.java
@@ -1,8 +1,10 @@
 package com.example.jpa_bank.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class DepositMoneyUserDto {
     private int moneyAmount;
     private int accountNumber;
--- src/main/java/com/example/jpa_bank/controller/dto/TransactionDto.java
+++ src/main/java/com/example/jpa_bank/controller/dto/TransactionDto.java
@@ -1,8 +1,10 @@
 package com.example.jpa_bank.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class TransactionDto {
     private int id;
     private int origen;
