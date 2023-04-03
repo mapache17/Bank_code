92a96baed41458692c989fb95feda502dc7618ca
Jhoan <84868357+Oswe-gif@users.noreply.github.com>
2023-04-02 19:01:01 -0500

    Remove unnecessary functions

--- src/main/java/com/example/jpa_bank/service/UserService.java
+++ src/main/java/com/example/jpa_bank/service/UserService.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -21,4 +22,5 @@ public class UserService {
     public List<AccountEntity> consultAccounts(int documentUser) {
         return accountRepository.getAllAccounts(documentUser);
     }
+
 }
