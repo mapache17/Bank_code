cd9a85e7f8dd59e23efe26e1a2451ff7c441fc1f
Jhoan <84868357+Oswe-gif@users.noreply.github.com>
2023-04-04 00:04:24 -0500

    Add integration test about checkBalance function

--- src/test/java/com/example/jpa_bank/integration/controller/AccountControllerTest.java
+++ src/test/java/com/example/jpa_bank/integration/controller/AccountControllerTest.java
@@ -15,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AccountControllerTest extends AbstractTest {
     private static final String PATH_CREATE_ACCOUNT = "/account/savings-account";
     private static final String PATH_CREATE_USER = "/user/savings-user";
+    private static final String PATH_CHECK_BALANCE = "/account/check-balance/";
     @Autowired
     private TestRestTemplate restTemplate;
     @Test
@@ -32,12 +33,35 @@ public class AccountControllerTest extends AbstractTest {
         AccountDto accountDto=null;
         UserDto userDto = new UserDto(1,"Jhoan","Ome","2025-03-24");
         restTemplate.postForEntity(PATH_CREATE_USER,userDto,UserEntity.class);
-        for (int i=0;i<=5;i++)
+        for (int i=0;i<=4;i++)
         {
             accountDto = new AccountDto(i,"Ahorro",0,"2025-03-24",userDto.getDocument());
             restTemplate.postForEntity(PATH_CREATE_ACCOUNT, accountDto, AccountEntity.class);
         }
         ResponseEntity<AccountEntity> accountEntityResponseEntity= restTemplate.postForEntity(PATH_CREATE_ACCOUNT, accountDto, AccountEntity.class);
         assertEquals(HttpStatusCode.valueOf(500), accountEntityResponseEntity.getStatusCode());
+    }
+    @Test
+    void Given_AnUserNOExist_When_Invoke_createAccount_Then_InternalServerError()
+    {
+        AccountDto accountDto = new AccountDto(1,"Ahorro",0,"2025-03-24",2);
+        ResponseEntity<AccountEntity> accountEntityResponseEntity= restTemplate.postForEntity(PATH_CREATE_ACCOUNT, accountDto, AccountEntity.class);
+        assertEquals(HttpStatusCode.valueOf(500), accountEntityResponseEntity.getStatusCode());
+    }
+    @Test
+    void Given_AnExistingIdAccount_When_Invoke_checkBalance_Then_ConsultBalance()
+    {
+        UserDto userDto = new UserDto(2,"Jhoan","Ome","2025-03-24");
+        restTemplate.postForEntity(PATH_CREATE_USER,userDto,UserEntity.class);
+        AccountDto accountDto = new AccountDto(1,"Ahorro",20,"2025-03-24",userDto.getDocument());
+        restTemplate.postForEntity(PATH_CREATE_ACCOUNT, accountDto, AccountEntity.class);
+        ResponseEntity<AccountEntity> accountEntityResponseEntity= restTemplate.getForEntity(PATH_CHECK_BALANCE+accountDto.getId(), AccountEntity.class);
+        assertEquals(new AccountEntity(1,"Ahorro",20,"2025-03-24",2),accountEntityResponseEntity.getBody());
+    }
+    @Test
+    void Given_AnIdAccountNOExist_When_Invoke_checkBalance_Then_InternalServerError()
+    {
+        ResponseEntity<AccountEntity> accountEntityResponseEntity= restTemplate.getForEntity(PATH_CHECK_BALANCE+1, AccountEntity.class);
+        assertEquals(HttpStatusCode.valueOf(500),accountEntityResponseEntity.getStatusCode());
     }
 }
