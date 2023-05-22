0b65db2b6309f723c7b3f82b1f9dfecff7270d10
Jhoan <84868357+Oswe-gif@users.noreply.github.com>
2023-05-21 21:58:56 -0500

    Add new profile for tests

--- src/test/java/com/example/jpa_bank/AbstractTest.java
+++ src/test/java/com/example/jpa_bank/AbstractTest.java
@@ -3,9 +3,11 @@ package com.example.jpa_bank;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(profiles = "test")
 public abstract class AbstractTest {
 
 }
